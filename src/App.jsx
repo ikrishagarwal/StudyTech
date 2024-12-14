96ad4054c9dd2a250c5844af16f38a35e2580246
Krish <68506145+ikrishagarwal@users.noreply.github.com>
2024-12-14 23:52:39 +0530

    feat: initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <section className="bg-slate-800 text-white h-screen w-screen flex items-center">
+        <p className="text-5xl font-bold w-full text-center">
+          <h1>Study Tech</h1>
+          <p className="text-lg p-6">Launching Soon..</p>
+        </p>
+      </section>
+    </>
+  );
+}
+
+export default App;
